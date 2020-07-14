319cf1be80d26a68099edc6a9012e2ff277db236
Vladimir Vikhanov <Vladimir.Spider@gmail.com>
2020-07-14 13:15:58 +0400

    query3

--- DynamicsCrm/Program.cs
+++ DynamicsCrm/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Query;
 using Microsoft.Xrm.Tooling.Connector;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace DynamicsCrm
@@ -39,6 +40,33 @@ namespace DynamicsCrm
                 {
                     Console.WriteLine("Connection failed...");
                 }
+
+                var query7 = new QueryExpression("account")
+                {
+                    ColumnSet = new ColumnSet(true),
+                    Criteria = new FilterExpression
+                    {
+                        Conditions = { new ConditionExpression("name", ConditionOperator.Equal, "TestOrganization")
+                                     }
+                    }
+
+                };
+                EntityCollection results7 = oServiceProxy.RetrieveMultiple(query7);
+
+
+                var query0 = new QueryExpression("connection")
+                {
+                    ColumnSet = new ColumnSet(true),
+                    Criteria = new FilterExpression
+                    {
+                        //FilterOperator = LogicalOperator.Or,
+                        Conditions = { new ConditionExpression("record1id", ConditionOperator.Equal, new Guid("011b5966-0dc5-ea11-a812-000d3ab416a6"))
+                                     }
+                    }
+
+                };
+
+                EntityCollection results0 = oServiceProxy.RetrieveMultiple(query0);
 
                 //ИЗВЛЕЧЬ ВСЕ КОНТАКТЫ, ЧЬЯ КОМПАНИЯ НАХОДИТСЯ В САМАРЕ
                 var query = new QueryExpression("contact")
@@ -77,7 +105,7 @@ namespace DynamicsCrm
                 //ИЗВЛЕЧЬ ВСЕ EMAIL ЗАДАННОГО ПОЛЬЗОВАТЕЛЯ, НАПИСАННЫЕ В ОПРЕДЕЛЕННУЮ КОМПАНИЮ
                 var query2 = new QueryExpression("email")
                 {
-                    ColumnSet = new ColumnSet("messageid"),
+                    ColumnSet = new ColumnSet("activityid"),
                     Criteria = new FilterExpression
                     {
                         FilterOperator = LogicalOperator.And,
@@ -89,17 +117,51 @@ namespace DynamicsCrm
                 };
 
                 EntityCollection results2 = oServiceProxy.RetrieveMultiple(query2);
-                Console.WriteLine("\nLIST OF MAIL'S ID:");
+                Console.WriteLine("\nLIST OF EMAIL'S ID:");
                 results2.Entities.ToList().ForEach(x =>
                 {
-                    Console.WriteLine(x.Attributes["messageid"]);
+                    Console.WriteLine(x.Attributes["activityid"]);
 
                 });
 
 
+                //ВЫВЕСТИ ВСЕ EMAIL-Ы, В КОТОРЫХ ТЕКУЩИЙ ПОЛЬЗОВАТЕЛЬ ЛИБО ПОЛУЧАТЕЛЬ/ЛИБО ОТПРАВИТЕЛЬ ПИСЬМА
+                QueryExpression query3 = new QueryExpression("email");
+                query3.ColumnSet = new ColumnSet("activityid", "to", "from");
+
+                LinkEntity EntityB = new LinkEntity("email", "activityparty", "activityid", "activityid", JoinOperator.Inner)
+                {
+                    Columns = new ColumnSet("activityid", "partyid", "participationtypemask"),
+                    EntityAlias = "ActivityParty",
+                    LinkCriteria = new FilterExpression
+                    {
+                        Conditions = { new ConditionExpression("participationtypemask", ConditionOperator.In, new object[] { "1", "2" }) }
+                    }
+                };
+
+                LinkEntity EntityC = new LinkEntity("activityparty", "systemuser", "partyid", "systemuserid", JoinOperator.Inner)
+                {
+                    Columns = new ColumnSet("fullname"),
+                    EntityAlias = "User"
+                };
+                EntityC.LinkCriteria.Conditions.Add(new ConditionExpression("fullname", ConditionOperator.Equal, "Владимир Виханов"));
+                EntityB.LinkEntities.Add(EntityC);
+                query3.LinkEntities.Add(EntityB);
+
+                EntityCollection results3 = oServiceProxy.RetrieveMultiple(query3);
+                Console.WriteLine("\nSECOND LIST OF EMAIL'S ID:");
+
+                var entities = from email in results3.Entities.ToList()
+                               group email by email.Attributes["activityid"] into g
+                               select new { id = g.Key };
+
+                foreach (var x in entities)
+                {
+                    Console.WriteLine(x.id);
+                };
+
                 Console.WriteLine("ОК");
             }
-
             catch (Exception ex)
             {
                 Console.WriteLine("Error - " + ex.ToString());
